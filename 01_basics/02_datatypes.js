64f088632bbaab9bb87e73d51cdac049048385cb
MohitSharma-08 <123964735+MohitSharma-08@users.noreply.github.com>
2024-11-18 14:44:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all js code as newer version
+
+// alert( 3 + 3 )   // we are using nodejs, not browser
+
+console.log(3+3) 
+console.log('Mohit')
+
+
+let name = "mohit"
+let age = 20
+let isLoggedIn = false
+
+// number    => 2 to power 53
+// bigint
+// string    => '' ""
+// boolean   => true/false
+// null      => standalone value, special value, not undefined just empty
+// undefined => 
+// symbol    => uniqueness 
+
+// object    => 
+
+console.log(typeof "mohit");
+console.log(typeof null);       // returns object
+console.log(typeof undefined);  // returns undefined
+
+//undefined is a type in itself but null is a object.
