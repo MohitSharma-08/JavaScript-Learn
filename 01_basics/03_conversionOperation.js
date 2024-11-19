6e78d8ee9d5d70cdafa52e27d5e85ac0a64e7478
MohitSharma-08 <123964735+MohitSharma-08@users.noreply.github.com>
2024-11-19 06:43:59 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score)); 
+
+
+let valueInNumber =  Number(score)
+// console.log(typeof valueInNumber);
+
+/* 
+but this method is not the best because it changes the type of anything 
+so we have to be careful while using this
+*/
+
+
+let num = "33abc"  // this is not a pure number
+let valNumber = Number(num)     // conversion
+// console.log(typeof valNumber);  // returns number
+// console.log(valNumber);       // returns NaN, which is also a special type 
+
+
+let isLoggedIn = 1      
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)     //converting to boolean
+// console.log(typeof booleanIsLoggedIn)           //returns type boolean
+// console.log(booleanIsLoggedIn)                  //returns true
+
+/*      during conversion
+if kept empty, returns false
+if string is passed, returns true 
+
+1   => true
+0   => false
+""  => false
+"a" => true
+
+*/
+
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(typeof stringNum); // type conversion into string
+console.log(stringNum)         //returns 33
