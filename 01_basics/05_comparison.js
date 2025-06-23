4b3368bb30cdeb615382ef9216fcc0ec52ee2d9d
Your Name <you@example.com>
2025-06-23 10:09:50 +0530

    comparison

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);  gives true/false
+
+console.log("2" > 1);  
+//js converted 2 to a number
+
+console.log("02" > 1); //here too
+
+
+// avoid these 
+console.log(null > 0 );
+console.log(null == 0);
+console.log(null >= 0);
+
+
