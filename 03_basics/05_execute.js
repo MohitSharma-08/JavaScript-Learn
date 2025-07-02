3c7b8f3523de01a5fc993269f36254e79d588ef7
Your Name <you@example.com>
2025-07-02 12:06:15 +0530

    execution bts

---
+++ 03_basics/05_execute.js
@@ -0,0 +1,19 @@
+/*
+js is single threaded
+
+
+    js runs in 2 phases
+    
+    first, global execution context is made, then it is referred to this variable
+
+    every envoirnment has diff execution context
+
+    two types of execution context - 
+        global ec
+        function ec
+
+
+    two phase, memory creation phase/creation phase
+
+    execution phase
+*/
