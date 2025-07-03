f6ff84354fbc889be4bf6882d57542e1c5788c13
Your Name <you@example.com>
2025-07-03 12:16:46 +0530

    while & do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while and do-while loop
+
+// let i = 0
+// while (i <= 10 ) {
+//     console.log(`Value of index is ${i}`);
+//     i += 2
+// }
+
+
+
+// let myArray = ["Flash", "Shaktiman", "Jack Sparrow"]
+
+// let arr = 0 
+// while (arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+
+//     arr += 1    
+// }
+
+
+// do while, runs at least one iteration and then checks the condition
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
